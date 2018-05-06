e484e1770769bd03085872e7378a5d3560e5ebf7
Aron Schueler <aronschueler@dass-it.de>
2018-05-06 23:28:13 +0200

    Add template for next PI2 project

---
+++ PraktischeInformatik2/Abgabe3/src/ArrayIntegerList.java
@@ -0,0 +1,38 @@
+
+public class ArrayIntegerList implements IntegerList{
+	protected int[] intArray;
+	protected int listLength;
+	@Override
+	
+	
+	public int getLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int insertLast(int value) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int getFirst() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int deleteFirst() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public boolean search(int value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+ 
+}
---
+++ PraktischeInformatik2/Abgabe3/src/IntegerList.java
@@ -0,0 +1,9 @@
+
+interface IntegerList {
+	int getLength();
+	int insertLast(int value);
+	int getFirst();
+	int deleteFirst();
+	boolean search(int value);
+	void print();
+}
