ee39ca0004048bc3334987d0fe957fddc4749407
Aron Schueler <aronschueler@dass-it.de>
2018-05-08 18:03:30 +0200

    Add/Replace four methods and constructor

--- PraktischeInformatik2/Abgabe3/src/ArrayIntegerList.java
+++ PraktischeInformatik2/Abgabe3/src/ArrayIntegerList.java
@@ -1,27 +1,40 @@
 
 public class ArrayIntegerList implements IntegerList{
-	protected int[] intArray;
-	protected int listLength;
+	private int[] intArray;
+	private int listLength;
+	
+	public ArrayIntegerList(int maxLength) {
+		this.intArray = new int[maxLength];
+		this.listLength = 0;
+	}
 	@Override
-	
-	
 	public int getLength() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.listLength;
 	}
 	@Override
 	public int insertLast(int value) {
-		// TODO Auto-generated method stub
+		if(this.intArray.length==this.listLength) {
+			return -1;
+		}
+		this.intArray[this.listLength++]=value;
 		return 0;
 	}
+
 	@Override
 	public int getFirst() {
-		// TODO Auto-generated method stub
-		return 0;
+		if(this.listLength==0) {
+			return -9999;
+		}
+		return this.intArray[0];
 	}
 	@Override
 	public int deleteFirst() {
-		// TODO Auto-generated method stub
+		if(this.listLength-1>0)
+		for(int i=1;i<this.listLength;i++) {
+			this.intArray[i-1]=this.intArray[i];
+		}
+		
+		this.listLength--;
 		return 0;
 	}
 	@Override
