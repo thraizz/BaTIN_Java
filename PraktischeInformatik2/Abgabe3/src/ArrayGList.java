cc8711a4895198f50bad630f6fb86ec83f5d5368
[Aron Schueler] <aron.schueler@dass-it.de>
2018-06-01 13:00:07 +0200

    Add ShowcaseClass as a demo for the generic list.
    Fixes Object types mistakenly used instead of generic types.
    Fixes some return values.

--- PraktischeInformatik2/Abgabe3/src/ArrayGList.java
+++ PraktischeInformatik2/Abgabe3/src/ArrayGList.java
@@ -1,11 +1,11 @@
 
-public class ArrayGList <T> implements GList{
+public class ArrayGList <T> implements GList <T>{
 	
-	private Object[] Array;
+	private T[] Array;
 	private int listLength;
 	
 	public ArrayGList(int maxLength) {
-		this.Array = new Object [maxLength];
+		this.Array = (T[]) new Object [maxLength];
 		this.listLength = 0;
 	}
 
@@ -15,7 +15,7 @@ public class ArrayGList <T> implements GList{
 		return this.listLength;
 	}
 
-	public Object insertLast(int value) {
+	public int insertLast(T value) {
 		if(this.Array.length==this.listLength) {
 			return -1;
 		}
@@ -27,32 +27,45 @@ public class ArrayGList <T> implements GList{
 		if(this.listLength==0) {
 			return -9999;
 		}
-		return this.Array[0];
+		return (T) this.Array[0];
 	}
-
-	public Object deleteFirst() {
-		if(this.listLength-1>0)
+	
+	public int deleteFirst() {
+		if(this.listLength-1>=0) {
 		for(int i=1;i<this.listLength;i++) {
 			this.Array[i-1]=this.Array[i];
 		}
-		
 		this.listLength--;
 		return 0;
+		}
+		return -1;
 	}
 
-	public boolean search(Object value) {
+	public boolean search(T value) {
 		for(int i=0;i<this.listLength;i++) {
-			if(this.Array[i]==value) {
-				return true;
-			}
+			if(this.Array[i]==value) {return false;}
 		}
-		return false;
+		return true;
 	}
 
 	public void print() {
+		if(this.listLength==0) {
+			System.out.println("Leer!");
+		}
 		for(int i=0;i<this.listLength;i++) {
 			System.out.println(this.Array[i]);
 			}
 		}
+	
+	public int append(T[] GArray) {
+		for(int i=0;i<GArray.length;i++) {
+			if(this.insertLast(GArray[i])==-1) { 
+				System.out.println("Maximale Groesse erreicht!");
+				return -1; 
+				}
+		}
+		return 0;
+		
+	}
 
 }
